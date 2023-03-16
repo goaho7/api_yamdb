f1cd2fadfd7c63fa7bbfd0fcc63e8f6c67ec6e38
Walkman-444 <117098330+Walkman-444@users.noreply.github.com>
2023-03-16 23:58:59 +0700

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,26 @@
+from reviews.models import Reviews, Category, Genre, Title
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Category, Genre, Title
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date')
+        model = Reviews
+
+    def validate(self, data):
+        if self.context['request'].method != 'POST':
+            return data
+        title = self.context['view'].kwargs.get('title_id')
+        author = self.context['request'].user
+        if Reviews.objects.filter(
+                author=author, title=title).exists():
+            raise serializers.ValidationError(
+                "Вы уже писали отзыв к этому произведению."
+            )
+        return data
 
 
 class CategorySerializer(serializers.ModelSerializer):

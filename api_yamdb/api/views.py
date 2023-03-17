a797999c3f3a28bc16950d35e1b174702b9c884a
Михаил <elstana@mail.ru>
2023-03-17 22:20:48 +0400

    Пуш Рейтинга, Урлов, и исправлений

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
-from api.serializers import ReviewsSerializer, CommentSerializer
+from api.serializers import ReviewSerializer, CommentSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
-from reviews.models import Category, Genre, Reviews, Title
+from reviews.models import Category, Genre, Review, Title
 from django.shortcuts import render
 from rest_framework import filters, viewsets, status
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializer, SignupSerializer, TokenSerializer, UserSerializer
@@ -23,14 +23,14 @@ from api_yamdb.settings import EMAIL
 User = get_user_model()
 
 
-class ReviewsViewSet(viewsets.ModelViewSet):
+class ReviewViewSet(viewsets.ModelViewSet):
     """Отзывы"""
-    serializer_class = ReviewsSerializer
+    serializer_class = ReviewSerializer
     pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        return title.Reviews.all()
+        return title.Review.all()
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -144,11 +144,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        review = get_object_or_404(Reviews, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Reviews, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
 
     def perform_update(self, serializer):
